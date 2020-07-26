ea4a8ce348a30b5adfc1a3d54166a219ebf2bac8
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-07-26 11:32:06 -0400

    Update float42s.jl

--- src/type/float42s.jl
+++ src/type/float42s.jl
@@ -12,6 +12,15 @@ end
 
 value(x::Float42) = x.val
 
-Base.Float64(x::Float42) = value(x)
-Base.Float32(x::Float42) = Float32(value(x))
-Base.Float16(x::Float42) = Float32(value(x))
+Base.BigFloat(x::Float42) = BigFloat(Float32(x))
+Base.Float64(x::Float42)  = Float64(Float32(x))
+Base.Float32(x::Float42)  = Float32(value(x))
+Base.Float16(x::Float42)  = Float16(value(x))
+
+Base.BigInt(x::Float42)) = BigInt(Float32(x))
+Base.Int128(x::Float42)) = Int128(Float32(x))
+Base.Int64(x::Float42))  = Int64(Float32(x))
+Base.Int32(x::Float42))  = Int32(Float32(x))
+Base.Int16(x::Float42))  = Int16(Float16(x))
+Base.Int8(x::Float42))   = Int8(Float32(x))
+
