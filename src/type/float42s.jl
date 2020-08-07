d02257a6ac3c3026447007e7533856c5377ed2e2
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-08-07 05:46:55 -0400

    Update float42s.jl

--- src/type/float42s.jl
+++ src/type/float42s.jl
@@ -21,8 +21,8 @@ Base.convert(::Type{Float32}, x::Float42) = Float32(value(x))
 
 Float42(x::Float64) = Float42(x)
 Base.convert(::Type{Float42}, x::Float64) = Float42(Float64(Float32(x)))
-Float64(x::Float42) = value(x)
-Base.convert(::Type{Float64}, x::Float42) = Float64(Float32(value(x)))
+Float64(x::Float42) = Float64(Float32(value(x)))
+Base.convert(::Type{Float64}, x::Float42) = value(x)
 
 Base.BigFloat(x::Float42) = BigFloat(Float32(x))
 Base.Float64(x::Float42)  = Float64(Float32(x))
@@ -35,4 +35,8 @@ Base.Int64(x::Float42)  = Int64(Float32(x))
 Base.Int32(x::Float42)  = Int32(Float32(x))
 Base.Int16(x::Float42)  = Int16(Float16(x))
 Base.Int8(x::Float42)   = Int8(Float32(x))
+
+
+Base.show(io::IO, x::Float42) = print(io, Float32(x))
+Base.parse(::Type{Float42}, x::String) = Float42(parse(Float32, x))
 
