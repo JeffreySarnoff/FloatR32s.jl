abc648c3d9f5110e9870a3b8aba19f463eeee430
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-08-11 12:49:48 -0400

    Update FloatR32s.jl

--- src/FloatR32s.jl
+++ src/FloatR32s.jl
@@ -173,7 +173,7 @@ rewrap(m::Matrix{ComplexR32}) =
     unsafe_wrap(Array{ComplexF64,2}, Ptr{ComplexF64}(pointer(m,1)), size(m))
 
 function __init__()
-  linalg = joinpath(Pkg.dir("FloatR32s"), "src", "linearalgebra.jl")
+  linalg = joinpath(@__DIR__, "linearalgebra.jl")
   if isdefined(Main, :LinearAlgebra)
       include(linalg)
   end
