9f445667c5d188be3ba4f23246d8e6a8de8e11b4
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-08-12 13:33:32 -0400

    Update primitive.jl

--- test/primitive.jl
+++ test/primitive.jl
@@ -5,6 +5,8 @@ sqrt_twoR32 = sqrt(twoR32)
 
 @testset "low level functions" begin
   @test hash(sqrt_two32) == hash(sqrt_twoR32)
+  @test string(sqrt_two32) == string(sqrt_twoR32)
+
   @test maxintfloat(Float32) == maxintfloat(Robust32)
   @test floatmin(Float32) == floatmin(Robust32)
   @test typemax(Float32) == typemax(Robust32)
