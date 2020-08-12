79ec734bf8faf9a8fb32e784398c7c3d14073b4d
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-08-12 14:40:45 -0400

    Update rewrap.jl

--- src/rewrap.jl
+++ src/rewrap.jl
@@ -17,57 +17,17 @@
 
 =#
 
-provide(m::Vector{Float64}) = reinterpret(Robust32, m)
-provide(m::Matrix{Float64}) = reinterpret(Robust32, m)
+for (R,F) in ((:Robust32, :Float64), (:ComplexR32, :ComplexF64))
+  for A in (:Vector, :Matrix)
+    @eval begin
+        provide(x::$A{$R}) = reinterpret($F, x)
+        provide(x::$A{$F}) = reinterpret($R, x)
 
-provide(m::Vector{Robust32}) = reinterpret(Float64, m)
-provide(m::Matrix{Robust32}) = reinterpret(Float64, m)
+        rewrap(x::$A{$R}) = unsafe_wrap($A{$F}, cvtptr($F, x), size(x))             
+        rewrap(x::$A{$F}) = unsafe_wrap($A{$R}, cvtptr($R, x), size(x))
+    end
+  end
+end
 
-provide(m::Vector{ComplexF64}) = reinterpret(ComplexR32, m)
-provide(m::Matrix{ComplexF4})  = reinterpret(ComplexR32, m)
-
-provide(m::Vector{ComplexR32}) = reinterpret(ComplexF64, m)
-provide(m::Matrix{ComplexR32}) = reinterpret(ComplexF64, m)
-
-
-@inline ptr(::Type{T}, m::Array{T,N}) where {N,T} = convert(Ptr{T}, pointer(m,1))
-
-rewrap(m::Vector{Float64}) =
-    unsafe_wrap(Array{Robust32,1}, ptr(Robust32, m), length(m))
-rewrap(m::Vector{Robust32}) =
-    unsafe_wrap(Array{Float64,1}, ptr(Float64, m), length(m))
-rewrap(m::Matrix{Float64}) =
-    unsafe_wrap(Array{Robust32,2}, ptr(Robust32, m), size(m))
-rewrap(m::Matrix{Robust32}) =
-    unsafe_wrap(Array{Float64,2}, ptr(Float64, m), size(m))
-
-rewrap(m::Vector{ComplexF64}) =
-    unsafe_wrap(Array{ComplexR32,1}, ptr(ComplexR32, m), length(m))
-rewrap(m::Vector{ComplexR32}) =
-    unsafe_wrap(Array{ComplexF64,1}, ptr(ComplexF64, m), length(m))
-rewrap(m::Matrix{ComplexF64}) =
-    unsafe_wrap(Array{ComplexR32,2}, ptr(ComplexR32, m), size(m))
-rewrap(m::Matrix{ComplexR32}) =
-    unsafe_wrap(Array{ComplexF64,2}, ptr(ComplexF64, m), size(m))
-
-#=
-
-rewrap(m::Vector{Float64}) =
-    unsafe_wrap(Array{Robust32,1}, Ptr{Robust32}(pointer(m,1)), length(m))
-rewrap(m::Vector{Robust32}) =
-    unsafe_wrap(Array{Float64,1}, Ptr{Float64}(pointer(m,1)), length(m))
-rewrap(m::Matrix{Float64}) =
-    unsafe_wrap(Array{Robust32,2}, Ptr{Robust32}(pointer(m,1)), size(m))
-rewrap(m::Matrix{Robust32}) =
-    unsafe_wrap(Array{Float64,2}, Ptr{Float64}(pointer(m,1)), size(m))
-
-rewrap(m::Vector{ComplexF64}) =
-    unsafe_wrap(Array{ComplexR32,1}, Ptr{ComplexR32}(pointer(m,1)), length(m))
-rewrap(m::Vector{ComplexR32}) =
-    unsafe_wrap(Array{ComplexF64,1}, Ptr{ComplexF64}(pointer(m,1)), length(m))
-rewrap(m::Matrix{ComplexF64}) =
-    unsafe_wrap(Array{ComplexR32,2}, Ptr{ComplexR32}(pointer(m,1)), size(m))
-rewrap(m::Matrix{ComplexR32}) =
-    unsafe_wrap(Array{ComplexF64,2}, Ptr{ComplexF64}(pointer(m,1)), size(m))
-
-=#    
+@inline cvtptr(::Type{T}, m::Array{T,N}) where {N,T} =
+    convert(Ptr{T}, pointer(m,1))
