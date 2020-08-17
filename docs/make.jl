ad9d47c96fd79fe958d0f0a32f31bec3e2d66362
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-08-17 16:02:10 -0400

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,11 +1,16 @@
 using Documenter, Robust32s
 
 makedocs(
+    modules = [Robust32s],
+    sitename = "Robust32s",
+    format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
     pages  = Any[
         "Overview" => "index.md",
+        "Methodology" => "methods.md"
     ]   
 )
         
 deploydocs(
     repo = "github.com/JeffreySarnoff/Robust32s.jl.git",
+    target = "build"
 )
