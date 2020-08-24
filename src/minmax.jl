4efec3b12ec199f6c134566b884fabe93ea0f81b
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-08-24 13:20:28 -0400

    Update and rename js_basics.jl to minmax.jl

---
+++ src/minmax.jl
@@ -0,0 +1,35 @@
+#=
+   High performance work-alike rewrites of `min`, `max`, `minmax`, `maxmin`.
+   
+   For use with `Float32`, `Float64`, and other IEEEFloats.
+=#
+   
+jsmin(a, b) = signbit(a-b) ? a : b + (a-a)
+jsmax(a, b) =  signbit(b-a) ? a : b + (a-a)
+jsminmax(a, b) = (jsmin(a,b), jsmax(a,b))
+jsmaminx(a, b) = (jsmax(a,b), jsmin(a,b))
+
+min(x::Robust32, y::Robust32) = Rob32(jsmin(value64(x), value64(y)))
+max(x::Robust32, y::Robust32) = Rob32(jsmax(value64(x), value64(y)))
+minmax(x::Robust32, y::Robust32) = Rob32(jsminmax(value64(x), value64(y)))
+maxmin(x::Robust32, y::Robust32) = Rob32(jsminmax(value64(x), value64(y)))
+
+min(x::Robust32, y::Float64) = Rob32(jsmin(value64(x), y))
+max(x::Robust32, y::Float64) = Rob32(jsmax(value64(x), y))
+minmax(x::Robust32, y::Float64) = Rob32(jsminmax(value64(x), y))
+maxmin(x::Robust32, y::Float64) = Rob32(jsminmax(value64(x), y))
+
+min(x::Float64, y::Robust32) = Rob32(jsmin(x, value64(y)))
+max(x::Float64, y::Robust32) = Rob32(jsmax(x, value64(y)))
+minmax(x::Float64, y::Robust32) = Rob32(jsminmax(x, value64(y)))
+maxmin(x::Float64, y::Robust32) = Rob32(jsminmax(x, value64(y)))
+
+min(x::Robust32, y::Real) = Rob32(jsmin(value64(x), Float64(y)))
+max(x::Robust32, y::Real) = Rob32(jsmax(value64(x), Float64(y)))
+minmax(x::Robust32, y::Real) = Rob32(jsminmax(value64(x), Float64(y)))
+maxmin(x::Robust32, y::Real) = Rob32(jsminmax(value64(x), Float64(y)))
+
+min(x::Real, y::Robust32) = Rob32(jsmin(Float64(x), value64(y)))
+max(x::Real, y::Robust32) = Rob32(jsmax(Float64(x), value64(y)))
+minmax(x::Real, y::Robust32) = Rob32(jsminmax(Float64(x), value64(y)))
+maxmin(x::Real, y::Robust32) = Rob32(jsminmax(Float64(x), value64(y)))
