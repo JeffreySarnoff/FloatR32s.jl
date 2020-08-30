377eaf327b35621bf0dbe009c0788fe7c5b35711
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-08-30 01:18:21 -0400

    Update Robust32s.jl

--- src/Robust32s.jl
+++ src/Robust32s.jl
@@ -244,15 +244,15 @@ end
 # fldmod(x,y) = divrem(x, y, RoundDown)
 divrem(x::Robust32, y::Robust32) = Rob32(divrem(value64(x), value64(y), RoundToZero))
 divrem(x::Robust32, y::Float64) = Rob32(divrem(value64(x), y, RoundToZero))
-divrem(x::Robust32, y::Float32) = Rob32(divrem(value64(x), Float64(y), RoundToZero))
+divrem(x::Robust32, y::Real) = Rob32(divrem(value64(x), Float64(y), RoundToZero))
 divrem(x::Float64, y::Robust32) = Rob32(divrem(x, value64(y), RoundToZero))
-divrem(x::Float32, y::Robust32) = Rob32(divrem(Float64(x), value64(y), RoundToZero))
+divrem(x::Real, y::Robust32) = Rob32(divrem(Float64(x), value64(y), RoundToZero))
 
 fldmod(x::Robust32, y::Robust32) = Rob32(fldmod(value64(x), value64(y), RoundDown))
 fldmod(x::Robust32, y::Float64) = Rob32(fldmod(value64(x), y, RoundDown))
-fldmod(x::Robust32, y::Float32) = Rob32(fldmod(value64(x), Float64(y), RoundDown))
+fldmod(x::Robust32, y::Real) = Rob32(fldmod(value64(x), Float64(y), RoundDown))
 fldmod(x::Float64, y::Robust32) = Rob32(fldmod(x, value64(y), RoundDown))
-fldmod(x::Float32, y::Robust32) = Rob32(fldmod(Float64(x), value64(y), RoundDown))
+fldmod(x::Real, y::Robust32) = Rob32(fldmod(Float64(x), value64(y), RoundDown))
  
 
 for F in (:hypot, :clamp)
