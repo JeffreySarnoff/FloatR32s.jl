86f9fec24fe94640ce9cb5d0fe41ee4a6d08cae7
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-09-08 22:03:30 -0400

    Update complex.jl

--- src/complex.jl
+++ src/complex.jl
@@ -3,3 +3,17 @@ flipsign(x::ComplexR32, y::T) where {T<:Real} = signbit(y) ? negate(x) : x
 
 Bool(x::ComplexR32) = iszero(x.im) ? isone(x.re) : false
 ComplexR32(x::Bool) = x ? one(ComplexR32) : zero(ComplexR32)
+
+const ComplexR32_0 = ComplexR32(Robust32_0, Robust32_0)
+const ComplexR32_1 = ComplexR32(Robust32_1, Robust32_0)
+const ComplexR32_2 = ComplexR32(Robust32_2, Robust32_0)
+
+zero(::Type{ComplexR32}) = ComplexR32_0
+one(::Type{ComplexR32})  = ComplexR32_1
+two(::Type{ComplexR32})  = ComplexR32_2
+
+iszero(x::ComplexR32) = x === ComplexR32_0
+isone(x::ComplexR32)  = x === ComplexR32_1
+istwo(x::ComplexR32)  = x === ComplexR32_2
+
+
