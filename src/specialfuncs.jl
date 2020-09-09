d4a7628e4c111e0e7b7db72dde0749ec13942b29
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-09-08 20:42:01 -0400

    Update specialfuncs.jl

--- src/specialfuncs.jl
+++ src/specialfuncs.jl
@@ -1,30 +1,6 @@
 import SpecialFunctions
 const SF = SpecialFunctions
 
-#=
-# with 1 arg Float64
-(:besselj0, 1, (Float64,))
-(:besselj1, 1, (Float64,))
-(:bessely0, 1, (Float64,))
-(:bessely1, 1, (Float64,))
-(:cosint, 1, (Float64,))
-(:dawson, 1, (Float64,))
-(:ellipe, 1, (Float64,))
-(:ellipk, 1, (Float64,))
-(:erf, 1, (Float64,))
-(:erfc, 1, (Float64,))
-(:erfcinv, 1, (Float64,))
-(:erfcx, 1, (Float64,))
-(:erfi, 1, (Float64,))
-(:erfinv, 1, (Float64,))
-(:gamma, 1, (Float64,))
-(:invdigamma, 1, (Float64,))
-(:logabsgamma, 1, (Float64,))
-(:logerfc, 1, (AbstractFloat,))
-(:logerfcx, 1, (AbstractFloat,))
-(:sinint, 1, (Float64,))
-=#
-
 for F in (:besselj0, :besselj1, :bessely0, :bessely1, 
           :cosint, :dawson, :ellipe, :ellipk, 
           :erf, :erfc, :erfcinv, :erfcx, :erfi, :erfinv, 
@@ -54,3 +30,8 @@ for F in (:besselj, :bessely)
   @eval SF.$F(x::Integer, y::Robust32) = Robust32(SF.$F(Int32(x), Float64(y))) 
 end
 
+for F in (:gamma_inc_inv)
+  @eval begin
+    SF.$F(x::Robust32, y::Robust32, z::Robust32) = Robust32(SF.$F(Float64(x), Float64(y), Float64(z)))
+  end                             
+end
